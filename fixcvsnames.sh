f27a9cd138542ae025176c0ce439dea9526b517b
David Mudrak <david.mudrak@gmail.com>
2010-11-30 14:55:09 +0100

    CVS usernames replaced with real names
    I used passwd database at cvs.moodle.org (by asking moodlebot via
    Jabber) and http://sourceforge.net/project/memberlist.php?group_id=30935
    as a source. In some cases, I used Google to find JIRA account or
    moodle.org profile to guess the real person.

---
+++ fixcvsnames.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+sed -i 's/^user:adamtpaw$/user:Adam Pawelczak/' moodle.log
+sed -i 's/^user:agrabs$/user:Andreas Grabs/' moodle.log
+sed -i 's/^user:ahmed_nabil$/user:Ahmed Nabil/' moodle.log
+sed -i 's/^user:amosbot$/user:AMOS bot/' moodle.log
+sed -i 's/^user:andersbe$/user:Anders Berggren/' moodle.log
+sed -i 's/^user:andreabix$/user:Andrea Bicciolo/' moodle.log
+sed -i 's/^user:andre_bln$/user:Andre Krüger/' moodle.log
+sed -i 's/^user:andro-bg$/user:Andrew Ivanov/' moodle.log
+sed -i 's/^user:anthonyforth$/user:Anthony Forth/' moodle.log
+sed -i 's/^user:anvergara$/user:Antonio Navarro/' moodle.log
+sed -i 's/^user:aparup$/user:Aparup Banerjee/' moodle.log
+sed -i 's/^user:arborrow$/user:Anthony Borrow/' moodle.log
+sed -i 's/^user:aredkin$/user:Andrew Redkin/' moodle.log
+sed -i 's/^user:ashleyholman$/user:Ashley Holman/' moodle.log
+sed -i 's/^user:aszora$/user:Istvan Bozsa/' moodle.log
+sed -i 's/^user:barias$/user:Benito Arias/' moodle.log
+sed -i 's/^user:bawjaws$/user:David Scotson/' moodle.log
+sed -i 's/^user:bobopinna$/user:Roberto Pinna/' moodle.log
+sed -i 's/^user:borinsky2407$/user:Christian Borowski/' moodle.log
+sed -i 's/^user:cap2501$/user:Ed Coyne/' moodle.log
+sed -i 's/^user:carlesbellver$/user:Carles Bellver/' moodle.log
+sed -i 's/^user:cataluke$/user:Luke Hudson/' moodle.log
+sed -i 's/^user:compuproggy$/user:Holger Schadeck/' moodle.log
+sed -i 's/^user:coolbit$/user:Zhang Dexuan/' moodle.log
+sed -i 's/^user:crbusch$/user:Chardelle Busch/' moodle.log
+sed -i 's/^user:csantossaenz$/user:Olga C Santos/' moodle.log
+sed -i 's/^user:c_schmitz$/user:Carsten Schmitz/' moodle.log
+sed -i 's/^user:danielfetzer$/user:Daniel Fetzer/' moodle.log
+sed -i 's/^user:danmarsden$/user:Dan Marsden/' moodle.log
+sed -i 's/^user:danstowell$/user:Dan Stowell/' moodle.log
+sed -i 's/^user:Dariem Garcs \\(Demian\\)$/user:Dariem Garcs/' moodle.log
+sed -i 's/^user:defacer$/user:Jon Papaioannou/' moodle.log
+sed -i 's/^user:dhawes$/user:Daryl Hawes/' moodle.log
+sed -i 's/^user:diml$/user:Valery Fremaux/' moodle.log
+sed -i 's/^user:djachenko$/user:Alex Djachenko/' moodle.log
+sed -i 's/^user:dlnsk$/user:Dmitry Pupinin/' moodle.log
+sed -i 's/^user:donal72$/user:Donal McMullan/' moodle.log
+sed -i 's/^user:donal$/user:Donal McMullan/' moodle.log
+sed -i 's/^user:dongsheng$/user:Dongsheng Cai/' moodle.log
+sed -i 's/^user:drabureesh$/user:Ali Abu-Reesh/' moodle.log
+sed -i 's/^user:dwoolhead$/user:Derek Woolhead/' moodle.log
+sed -i 's/^user:ecastrolt$/user:Enrique Castro/' moodle.log
+sed -i 's/^user:ericmerrill$/user:Eric Merrill/' moodle.log
+sed -i 's/^user:ethem evlice$/user:Ethem Evlice/' moodle.log
+sed -i 's/^user:ethem$/user:Ethem Evlice/' moodle.log
+sed -i 's/^user:exe-cutor$/user:Lukas Haemmerle/' moodle.log
+sed -i 's/^user:fiedorow$/user:Zbigniew Fiedorowicz/' moodle.log
+sed -i 's/^user:flipman$/user:Girts Ozolins/' moodle.log
+sed -i 's/^user:fmarier$/user:Francois Marier/' moodle.log
+sed -i 's/^user:fyang1024$/user:Fei Yang/' moodle.log
+sed -i 's/^user:garethmorgan$/user:Gareth Morgan/' moodle.log
+sed -i 's/^user:gbateson$/user:Gordon Bateson/' moodle.log
+sed -i 's/^user:giro123$/user:Claudio Tavares/' moodle.log
+sed -i 's/^user:gniske$/user:Kristian Nielsen/' moodle.log
+sed -i 's/^user:gustav_delius$/user:Gustav Delius/' moodle.log
+sed -i 's/^user:hansdezwart$/user:Hans de Zwart/' moodle.log
+sed -i 's/^user:havran$/user:Juro Chlebec/' moodle.log
+sed -i 's/^user:iarenaza$/user:Iñaki Arenaza/' moodle.log
+sed -i 's/^user:ikawhero$/user:Shane Elliott/' moodle.log
+sed -i 's/^user:Inaki$/user:Iñaki Arenaza/' moodle.log
+sed -i 's/^user:jamiesensei$/user:James Pratt/' moodle.log
+sed -i 's/^user:jbeedell$/user:John Beedell/' moodle.log
+sed -i 's/^user:jerome$/user:Jerome Mouneyrac/' moodle.log
+sed -i 's/^user:jerome mouneyrac$/user:Jerome Mouneyrac/' moodle.log
+sed -i 's/^user:jgraham909$/user:Jeff Graham/' moodle.log
+sed -i 's/^user:jgutierr25$/user:Jérôme Gutierrez/' moodle.log
+sed -i 's/^user:jmg324$/user:Jenny Gray/' moodle.log
+sed -i 's/^user:johncaptain$/user:John Eyre/' moodle.log
+sed -i 's/^user:john St$/user:John Stabinger/' moodle.log
+sed -i 's/^user:jonathanharker$/user:Jonathan Harker/' moodle.log
+sed -i 's/^user:jpuntd$/user:Jan Dierckx/' moodle.log
+sed -i 's/^user:julmis$/user:Janne Mikkonen/' moodle.log
+sed -i 's/^user:jungwirr$/user:Russell Jungwirth/' moodle.log
+sed -i 's/^user:kaipe$/user:Henrik Kaipe/' moodle.log
+sed -i 's/^user:koenr$/user:Koen Roggemans/' moodle.log
+sed -i 's/^user:lamsdev$/user:Anthony Sukkar/' moodle.log
+sed -i 's/^user:lapsa$/user:M Hagman/' moodle.log
+sed -i 's/^user:lashaalt$/user:Lasha Altunashvili/' moodle.log
+sed -i 's/^user:les_kopari$/user:Les Kopari/' moodle.log
+sed -i 's/^user:lfrodrigues$/user:Luis Rodrigues/' moodle.log
+sed -i 's/^user:lonecatz$/user:Ling Li/' moodle.log
+sed -i 's/^user:luizlaydner$/user:Luiz Laydner/' moodle.log
+sed -i 's/^user:mark-nielsen$/user:Mark Nielsen/' moodle.log
+sed -i 's/^user:martignoni$/user:Nicolas Martignoni/' moodle.log
+sed -i 's/^user:martin$/user:Martin Dougiamas/' moodle.log
+sed -i 's/^user:martinlanghoff$/user:Martin Langhoff/' moodle.log
+sed -i 's/^user:mattc-catalyst$/user:Matt Clarkson/' moodle.log
+sed -i 's/^user:mchampan$/user:Michael Champanis/' moodle.log
+sed -i 's/^user:mchurch$/user:Mike Churchward/' moodle.log
+sed -i 's/^user:michaelpenne$/user:Michael Penney/' moodle.log
+sed -i 's/^user:mikewaters$/user:Mike Waters/' moodle.log
+sed -i 's/^user:miksik$/user:Daniel Miksik/' moodle.log
+sed -i 's/^user:mindforge$/user:Julian Sedding/' moodle.log
+sed -i 's/^user:minkowski$/user:M Minkowski/' moodle.log
+sed -i 's/^user:mits$/user:Mitsuhiro Yoshida/' moodle.log
+sed -i 's/^user:mjollnir_$/user:Penny Leach/' moodle.log
+sed -i 's/^user:Moodle HQ git importer$/user:gitbot/' moodle.log
+sed -i 's/^user:moodler$/user:Martin Dougiamas/' moodle.log
+sed -i 's/^user:moodlerobot$/user:Moodle Robot/' moodle.log
+sed -i 's/^user:moquist$/user:Matt Oquist/' moodle.log
+sed -i 's/^user:msketcham$/user:Michael Ketcham/' moodle.log
+sed -i 's/^user:mudrd8mz$/user:David Mudrak/' moodle.log
+sed -i 's/^user:nfreear$/user:Nick Freear/' moodle.log
+sed -i 's/^user:nicolasconnault$/user:Nicolas Connault/' moodle.log
+sed -i 's/^user:paca70$/user:Petri Asikainen/' moodle.log
+sed -i 's/^user:palojacko$/user:Pavol Jackulik/' moodle.log
+sed -i 's/^user:papita$/user:Paula de Waal/' moodle.log
+sed -i 's/^user:patrickslee$/user:Patrick Lee/' moodle.log
+sed -i 's/^user:Penelope Leach$/user:Penny Leach/' moodle.log
+sed -i 's/^user:peterbulmer$/user:Peter Bulmer/' moodle.log
+sed -i 's/^user:petcheverry$/user:Pablo Etcheverry/' moodle.log
+sed -i 's/^user:phoenixfr$/user:Romuald Lorthioir/' moodle.log
+sed -i 's/^user:piers$/user:Piers Harding/' moodle.log
+sed -i 's/^user:pigui$/user:Jordi Piguillem/' moodle.log
+sed -i 's/^user:pichetp$/user:Pierre Pichet/' moodle.log
+sed -i 's/^user:pilpi$/user:Olli Savolainen/' moodle.log
+sed -i 's/^user:poltawski$/user:Dan Poltawski/' moodle.log
+sed -i 's/^user:ptrkmkl$/user:Patrick Malley/' moodle.log
+sed -i 's/^user:ralf-bonn$/user:Ralf Hilgenstock/' moodle.log
+sed -i 's/^user:rcantada$/user:Roel Cantada/' moodle.log
+sed -i 's/^user:rezaie9$/user:Shamim Rezaie/' moodle.log
+sed -i 's/^user:rkingdon$/user:Ray Kingdon/' moodle.log
+sed -i 's/^user:robertall$/user:Robert Allerstorfer/' moodle.log
+sed -i 's/^user:rwijaya$/user:Rossiani Wijaya/' moodle.log
+sed -i 's/^user:rtcn2$/user:Rod Norfor/' moodle.log
+sed -i 's/^user:samhemelryk$/user:Sam Hemelryk/' moodle.log
+sed -i 's/^user:sam_marshall$/user:Sam Marshall/' moodle.log
+sed -i 's/^user:samulik$/user:Samuli Karevaara/' moodle.log
+sed -i 's/^user:scop22$/user:Timothy Allen/' moodle.log
+sed -i 's/^user:scyrma$/user:Mathieu Petit-Clair/' moodle.log
+sed -i 's/^user:selliott$/user:Scott Elliott/' moodle.log
+sed -i 's/^user:set_lo$/user:Set Lo/' moodle.log
+sed -i 's/^user:skaldrom$/user:Michael Schneider/' moodle.log
+sed -i 's/^user:skodak$/user:Petr Skoda/' moodle.log
+sed -i 's/^user:stigbjarne$/user:Stig Bjarne Haugen/' moodle.log
+sed -i 's/^user:stronk7$/user:Eloy Lafuente/' moodle.log
+sed -i 's/^user:thepurpleblob$/user:Howard Miller/' moodle.log
+sed -i 's/^user:thetrinity$/user:Paul Shew/' moodle.log
+sed -i 's/^user:thompson697$/user:Al Thompson/' moodle.log
+sed -i 's/^user:tjhunt$/user:Tim Hunt/' moodle.log
+sed -i 's/^user:tomaz$/user:Tomaz Savodnik/' moodle.log
+sed -i 's/^user:tomflannaghan$/user:Tom Flannaghan/' moodle.log
+sed -i 's/^user:toyomoyo$/user:Yu Zhang/' moodle.log
+sed -i 's/^user:trobb$/user:Thomas Robb/' moodle.log
+sed -i 's/^user:ueu_ueu$/user:Santurtziko Udal Euskaltegia/' moodle.log
+sed -i 's/^user:urs_hunkler$/user:Urs Hunkler/' moodle.log
+sed -i 's/^user:villate$/user:Jaime Villate/' moodle.log
+sed -i 's/^user:vinkmar$/user:Vince Marcovecchio/' moodle.log
+sed -i 's/^user:vthung$/user:Vu Hung/' moodle.log
+sed -i 's/^user:vyshane$/user:Vy-Shane Sin Fat/' moodle.log
+sed -i 's/^user:whchuang$/user:Wen Hao Chuang/' moodle.log
+sed -i 's/^user:wildgirl$/user:Helen Foster/' moodle.log
+sed -i 's/^user:willcast$/user:Williams Castillo/' moodle.log
+sed -i 's/^user:zibas$/user:Ziba S/' moodle.log
